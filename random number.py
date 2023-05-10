8cda852e32d9e86ff304faf46a6b6f2805ac6ecd
not-wowmagic <gmawong10@gmail.com>
2023-05-10 20:01:38 +0800

    yes

--- random number.py
+++ random number.py
@@ -1,6 +1,6 @@
 import random
 
-top_of_range = input("Type a number: ")
+top_of_range = input("Type the maximum number: ")
 
 if top_of_range.isdigit():
     top_of_range = int(top_of_range)
@@ -12,6 +12,25 @@ else:
     print('Please type a number larger than 0 next time.')
     quit()
 
-r = random.randint(1, 100)
-print (r)
+random_number = random.randint(0, top_of_range)
+guesses = 0
 
+while True:
+    guesses += 1
+    user_guess = input("Make a guess:")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print('Please type a number next time.')
+        continue
+
+    if user_guess == random_number:
+        print("You got it!")
+        break
+    elif user_guess > random_number:
+            print("Lower!!!")
+    else:
+            print("Higher!!!")
+        
+print("Nahulaan mo rin in", guesses, "guesses")
+print("ey")
